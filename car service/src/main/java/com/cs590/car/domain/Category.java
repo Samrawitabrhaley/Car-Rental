ec85479421226cf2c30e6f631f41894004b996c4
HenokWelday <93111848+HenokWelday@users.noreply.github.com>
2022-06-10 18:11:46 -0500

    created car service

---
+++ car service/src/main/java/com/cs590/car/domain/Category.java
@@ -0,0 +1,7 @@
+package com.cs590.car.domain;
+
+public class Category {
+
+    private Long id;
+    private CategoryType categoryType;
+}
---
+++ car service/src/main/java/com/cs590/car/service/CarServiceImpl.java
@@ -0,0 +1,4 @@
+package com.cs590.car.service;
+
+public class CarServiceImpl {
+}
