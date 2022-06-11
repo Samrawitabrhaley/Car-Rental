db4da43ce578684a52effc3b3110017ad1fff218
nahomdebretsion <nahomdebretsion@gmail.com>
2022-06-11 01:22:53 -0500

    payment finished

---
+++ cc-payment/src/main/java/com/ccpayment/ccpaayment/dto/Address.java
@@ -0,0 +1,8 @@
+package com.ccpayment.ccpaayment.dto;
+
+public class Address {
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+}
--- cc-payment/src/main/java/com/ccpayment/ccpaayment/repository/CreditCardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ccpayment.ccpaayment.repository;
-
-import com.ccpayment.ccpaayment.model.CreditCard;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CreditCardRepository extends JpaRepository<CreditCard,Long> {
-}
